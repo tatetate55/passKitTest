1f3e684faa01d1f24572d2f9c61cb34a7d4e7274
KAMAKURAKAZUHIRO <ruteinyoyokk@gmail.com>
2019-04-09 22:32:34 +0900

    first commit

---
+++ hogePay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hogePay
+//
+//  Created by KAMAKURAKAZUHIRO on 2019/04/09.
+//  Copyright © 2019 KAMAKURAKAZUHIRO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func add(_ sender: Any) {
+        
+        
+    }
+    
+}
+
